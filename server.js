8c0a8653bdd9e599b086ea4ab44acc4099ee8fb9
chaithanyas90 <chaithuintimetec@gmail.com>
2019-02-20 09:38:07 +0530

    Added routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express"),
     app = new express();
 app.use(express.static('dist'));
+	app.get ('/', (req, res) => res.send('Hello world'));
+	app.get ('/home', (req, res) => res.send('Home page'));
 app.listen(8000,function(){
+
 	console.log("app started on port 8000")
 })
